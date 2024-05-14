84f68150cb31bf2547fb5055f7ebb8eade54bebe
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-05-14 11:18:18 -0300

    update

--- aula-14-05/index.php
+++ aula-14-05/index.php
@@ -13,31 +13,57 @@
         <pre> 
             <?php
 
-            $a1 = array("antonio", "luis", "jose", "joao");
-            echo $a1[0]." ";
-            echo $a1[1]." ";
-            echo $a1[2]." ";
-            echo $a1[3]." ";
+                $a1 = array("antonio", "luis", "jose", "joao");
+                echo $a1[0]." ";
+                echo $a1[1]." ";
+                echo $a1[2]." ";
+                echo $a1[3]." ";
 
             ?> 
         </pre>
         <h2>Exemplo 2 - usando for()</h2>
         <pre> 
             <?php
-            $a2 = array(10,9,8,7);
-            for($i=0;$i<count($a2);$i++){
-                echo $a2[$i]." <br> ";
-            }
+                $a2 = array(10,9,8,7);
+                for($i=0;$i<count($a2);$i++){
+                    echo $a2[$i]." <br> ";
+                }
             ?> 
         </pre>
-        <h2>Exemplo 3</h2>
+        <h2>Exemplo 3 - usando foreach()</h2>
         <pre> 
             <?php
-            $a3 = array("roberto",20,"guanambi",7);
-            foreach ($a3 as $chave => $valor) {
-                echo $chave . " - " . $valor . "<br>";
-            }
+                $a3 = array("nome"=>"roberto","idade"=>20,"cidade"=>"guanambi","id"=>7);
+                foreach ($a3 as $chave => $valor) {
+                    echo $chave . " - " . $valor . "<br>";
+                }
             ?> 
+        </pre>
+        <h2>Exemplo 4 -</h2>
+        <pre> 
+            <?php
+                $alunos = array("maria"=>
+                                        array("endereco" => "rua chile 1046",
+                                            "bairro" => "centro",
+                                            "cidade" => "pesqueiro"),
+                                "joao" =>
+                                        array("endereco" => "rua iapo 245",
+                                            "bairro" => "vomitamel",
+                                            "cidade" => "guanambi"),
+                                "zeca" =>
+                                        array("endereco" => "rua iapo 295",
+                                            "bairro" => "vomitamel",
+                                            "cidade" => "guanambi")
+                                );
+                foreach ($alunos as $nome => $valor) {
+                    echo $nome." - ".$valor."<br>";
+                    /*
+                    foreach ($valor as $key => $value) {
+                        echo $key." - ".$value."<br>";
+                    }
+                    */
+                }
+            ?>  
         </pre>
     </div>
     
