3c5196ff1e21eb3db60504110b6cee64c3e12508
Rian Cesar Oliveira Souza <riancesar.contact@gmail.com>
2024-07-23 08:42:35 -0300

    update

--- atividade/viewRoute.php
+++ atividade/viewRoute.php
@@ -14,21 +14,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--bootstrap -->
-    <link rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script
-    src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
-    <script
-    src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>Document</title>
 </head>
 <body>
     <form action="viewRoute.php" method="post">
         <input TYPE="hidden" NAME="form_submit" VALUE="OK">
         <div class='col-sm-4'>
-            <button type="submit" name="acao" value="cadastar/0" class="btn btnprimary btn-block">
+            <button type="submit" name="acao" value="cadastar/0" class="btn btn-primary btn-block">
             <b>Cadastar</b> </button>
         </div>
         <div class='col-sm-4'>
